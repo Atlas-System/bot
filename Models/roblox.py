8c3b55225cf046f86bf277b502c0c9815d0c4f75
markination <holder.invite@gmail.com>
2025-05-18 10:35:13 +0100

    Bloxlink

--- Models/roblox.py
+++ Models/roblox.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 import aiohttp
+import os
 
 class RobloxUser(BaseModel):
     id: int
@@ -10,9 +11,19 @@ class RobloxUser(BaseModel):
 
 async def fetch_roblox_user(user_id: int) -> RobloxUser:
     async with aiohttp.ClientSession() as session:
-        async with session.get(f"https://users.roblox.com/v1/isers/{user_id}") as response:
+        async with session.get(f"https://users.roblox.com/v1/users/{user_id}") as response:
             if response.status == 200:
                 data = await response.json()
                 return RobloxUser(**data)
             else:
                 raise Exception(f"Failed to fetch user: {response.status}")
+
+async def fetch_bloxlink_user(discord_user_id: int) -> RobloxUser:
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url=f"https://api.blox.link/v4/public/discord-to-roblox/:{discord_user_id}", headers={"Authorization": os.getenv("BLOXLINK_API_KEY")}) as response:
+            if response.status == 200:
+                data = await response.json()
+                roblox_user = await fetch_roblox_user(data['robloxId'])
+                return roblox_user
+            else:
+                raise Exception(response.text())
